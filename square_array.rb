3ad67ff5e53d79401d83d162000e6623883e3a6d
Laura Darlak <laura@polyporedesign.com>
2018-08-17 13:45:36 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,20 +1,21 @@
+
 # square array using each iterator
+#
+# def square_array(array)
+#   # create new array
+#   new_array = []
+#   # iterate over each number in the array
+#   array.each do |number|
+#     # square each number in the array and save value to a new variable
+#     new_number = number**2
+#     # shovel squared number variable into the new, empty array
+#     new_array << new_number
+#   end
+#   # return new array
+#   new_array
+# end
 
-def square_array(array)
-  # create new array
-  new_array = []
-  # iterate over each number in the array
-  array.each do |number|
-    # square each number in the array and save value to a new variable
-    new_number = number**2
-    # shovel squared number variable into the new, empty array
-    new_array << new_number
-  end
-  # return new array
-  new_array
-end
-
-# refactor
+# refactored
 def square_array(array)
   squared = []
   array.each do |element|
